81ed64ae090238c784dc7c4749e174375889e150
José Roberto Nathanael Ríos Nuñez <rober_leanatan@hotmail.com>
2014-12-05 15:48:41 -0700

    Se crea la clase ProductCatalogData que se encarga de declarar la tabla hash de los productos y los precios

--- Examen/src/domain/ProductCatalog.java
+++ Examen/src/domain/ProductCatalog.java
@@ -3,23 +3,23 @@ package domain;
 import java.util.*;
 
 public class ProductCatalog {
-  private Hashtable productSpecifications = new Hashtable();
+  private ProductCatalogData data = new ProductCatalogData(new Hashtable());
 
-  public ProductCatalog() {
+public ProductCatalog() {
     ProductSpecification ps =
         new ProductSpecification(1, 100, "product 1");
-    productSpecifications.put(new Integer(1), ps);
+    data.productSpecifications.put(new Integer(1), ps);
     ps = new ProductSpecification(2, 200, "product 2");
-    productSpecifications.put(new Integer(2), ps);
+    data.productSpecifications.put(new Integer(2), ps);
 
     ps = new ProductSpecification(3, 300, "product 3");
-    productSpecifications.put(new Integer(3), ps);
+    data.productSpecifications.put(new Integer(3), ps);
     ps = new ProductSpecification(4, 400, "product 4");
-    productSpecifications.put(new Integer(4), ps);
+    data.productSpecifications.put(new Integer(4), ps);
 
   }
 
   public ProductSpecification specification(int upc) {
-    return (ProductSpecification) productSpecifications.get(new Integer(upc));
+    return (ProductSpecification) data.productSpecifications.get(new Integer(upc));
   }
 }
---
+++ Examen/src/domain/ProductCatalogData.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.Hashtable;
+
+public class ProductCatalogData {
+	public Hashtable productSpecifications;
+
+	public ProductCatalogData(Hashtable productSpecifications) {
+		this.productSpecifications = productSpecifications;
+	}
+}

f27ce0241280799edba18319fb7f1c007924eee3
Luis Eduardo Villela Zavala <luisedo21@gmail.com>
2014-12-05 15:34:01 -0700

    Comienza poyecto con la importación del código original y la creación de la prueba unitaria

---
+++ Examen/src-pruebas/paquetedepruebas/nula.java
@@ -0,0 +1,14 @@
+package paquetedepruebas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class nula {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Examen/src-pruebas/paquetedepruebas/p.java
@@ -0,0 +1,38 @@
+package paquetedepruebas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import workflow.POST;
+import domain.Store;
+
+public class p {
+	Store t;
+	POST p;
+	
+	@Before
+	public void setUp() throws Exception {
+		t = new Store();
+		p = t.getPOST();
+	}
+	
+	@Test
+	public void PU() 
+	{
+		System.out.println("Capturo 1 articulo (COD=1) de a 100 pesos");
+		p.EnterItem(1,1);
+		System.out.println("Capturo 2 articulos (COD=2) de a 200 pesos");
+		p.EnterItem(2,2);
+		System.out.println("Capturo 1 articulo (COD=1) de a 100 pesos");
+		p.EnterItem(1,1);
+		System.out.println("Finalizo la venta...");
+		p.endSale();
+		System.out.println("Total de la venta..."+p.getSale().total());
+		System.out.println("Pago con 650 pesos...");
+		p.makePayment(650);
+		System.out.println("La feria es: ..."+ p.getSale().getBalance());
+		assertTrue(p.getSale().getBalance()==50.0);
+	}
+}
